3275fa543e35ebefd0f6e81d6cfdc23a243d8324
DiDoki <151767044+DiDoki@users.noreply.github.com>
2023-12-02 14:10:44 +0300

    version_1

---
+++ script.js
@@ -0,0 +1,9 @@
+// function Rezult(){let rez= document.getElementById('input').textContent;
+// return rez;}
+// btn1.onclick = function(){
+//     document.getElementById('input').innerHTML=1;
+// }
+
+
+
+
