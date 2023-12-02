b4faf7b2ac5f98ac4e36aa975fb5eecdb9902175
DiDoki <151767044+DiDoki@users.noreply.github.com>
2023-12-02 14:17:03 +0300

    Add files via upload

--- script.js
+++
@@ -1,9 +0,0 @@
-// function Rezult(){let rez= document.getElementById('input').textContent;
-// return rez;}
-// btn1.onclick = function(){
-//     document.getElementById('input').innerHTML=1;
-// }
-
-
-
-
